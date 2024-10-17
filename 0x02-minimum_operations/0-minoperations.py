4bd56c2013f9a2aadc569db4e7d0039a745be29a
LoayAbassi <abassi.loay@gmail.com>
2024-10-17 19:25:22 +0100

    hoping:(

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,11 +13,14 @@ def minOperations(n: int) -> int:
     """
     if n <= 1:
         return 0
-    divisor: int = 2
+    current_length: int = 1
     operations: int = 0
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
+    while current_length < n:
+        if n % current_length == 0:
+            operations += 1
+            current_length *= 2
+        else:
+            operations += 2
+            current_length += 1
+
     return operations
