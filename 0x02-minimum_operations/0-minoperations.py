2e596bba375196cac4f4a2f7bd9ff6af44a9265b
LoayAbassi <abassi.loay@gmail.com>
2024-10-17 22:37:18 +0100

    now ?

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python3
-"""contains a minoperations function"""
+"""
+contains a minoperations function
+"""
 
 
 def minOperations(n):
     """
-    Args:
-        n (int): goal Hs
-
-    Returns:
-        int: _description_
+    returns minimum operations
     """
     if n <= 1:
         return 0
 
-    divisor= 2
-
-    operations= 0
+    divisor = 2
+    operations = 0
 
     while n > 1:
         if n % divisor == 0:
             operations += divisor
-            n //= divisor
+            n /= divisor
 
         else:
             divisor += 1
