e7028a6a1043b9ff80eb609b5a05788801bd2220
LoayAbassi <abassi.loay@gmail.com>
2024-10-17 19:03:36 +0100

    maybe now

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,14 +12,15 @@ def minOperations(n: int) -> int:
     if n <= 1:
         return 0
 
-    divisor: int = 2
+    root: int = 2
 
     operations: int = 0
 
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n //= divisor
-        divisor += 1
+    while root <= n:
+        if n % root == 0:
+            ops += root
+            n = n / root
+            root -= 1
+        root += 1
 
     return operations
