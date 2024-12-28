de82c27307248cbf6f466df36251b399f4db44a6
LoayAbassi <abassi.loay@gmail.com>
2024-12-28 20:47:17 +0100

    more brain burns

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+contains makechange function
+"""
+
+
+def makeChange(coins, total):
+    """
+    Args:
+        coins, total 
+    """
+    if total <= 0:
+        return 0
+    dp = [0] + [float("inf")] * (total)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != float("inf") else -1
